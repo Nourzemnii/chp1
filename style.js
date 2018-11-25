0b8cf89ed2ed8f436e908d6040a203daac0b50cb
Nourzemnii <44751254+Nourzemnii@users.noreply.github.com>
2018-11-25 18:05:04 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+function bold() {
+    document.getElementById("paragraph").style.fontWeight = "bold";
+}
+function normal(){
+    document.getElementById("paragraph").style.fontStyle = 'normal';
+    document.getElementById("paragraph").style.fontWeight = 'normal';
+
+}
+function italic() {
+   
+    document.getElementById("paragraph").style.fontStyle = 'italic';
+}
+
+function police() {
+    var police = document.getElementById('pl').value
+    document.getElementById("paragraph").style.fontFamily = police;
+}
+
+function taille() {
+    var size=document.getElementById('sz').value;
+    document.getElementById("paragraph").style.fontSize = size+"px";
+}
